ff48d258cec4216ab2ede3381acb81d652d2fc87
KRIMZON\krimzon <morales.edinson@outlook.com>
2023-11-03 13:02:34 -0500

    update

--- src/routes/companies.py
+++ src/routes/companies.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, status
-from src.database import get_db
-from src.models import models
+# from fastapi import APIRouter, Depends, status
+# from src.database import get_db
+# from src.models import models
 # from sqlalchemy.orm import Session
 # from src.schemas.response import ResponseDto
-from fastapi.encoders import jsonable_encoder
+# from fastapi.encoders import jsonable_encoder
 
-router = APIRouter()
+# router = APIRouter()
 
 # @router.get("/{company_id}", response_model=ResponseDto, status_code=status.HTTP_200_OK)
 # async def get_company(company_id:int, db: Session = Depends(get_db)):
